a992cce8762350d44acdd2f03341106392796878
Anri Liy <anri@realquantum.com>
2020-02-06 09:10:04 -0800

    Submit Feedback Form

---
+++ tests/submit-feedback.test.js
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+
+describe('Submit feedback Form Test', () => {
+  before(function() {
+    browser.url('http://zero.webappsecurity.com/index.html');
+
+    const metadata = browser.getMetadata();
+    expect(metadata.title).to.contains('Personal Banking');
+    expect(metadata.url).to.contains('index.html');
+  });
+
+  it ('should navigate to feedback page', () => {
+    $('#feedback').waitForExist(5000);
+    $('#feedback').click();
+
+    const metadata = browser.getMetadata();
+    expect(metadata.url).to.contains('feedback.html');
+  });
+
+  it('should submit feedback form', () => {
+    $('form').waitForExist(5000);
+    $('#name').setValue('Sarah');
+    $('#email').setValue('sarah@mail.com');
+    $('#subject').setValue('Subject');
+    $('#comment').setValue('Comments');
+    $('.btn-primary').click();
+  });
+
+  it('should show success feedback page',() => {
+    $('#feedback-title').waitForExist(5000);
+  })
+})
